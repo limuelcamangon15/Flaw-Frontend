d5707943e0a366cbfbf047248cc48d9e5d04bc4f
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-09 23:36:04 +0800

    update: created a component for teamcard

---
+++ app/components/team/TeamCard.tsx
@@ -0,0 +1,33 @@
+import { ChevronRight, Users } from "lucide-react";
+
+interface TeamCardProps {
+  id: number;
+  teamName: string;
+  members: number;
+}
+
+export default function TeamCard({ id, teamName, members }: TeamCardProps) {
+  return (
+    <div className="relative font-sans group overflow-hidden w-full md:max-w-70 max-h-40 min-h-30 cursor-pointer rounded-2xl border border-neutral-800 bg-neutral-900 p-4 transition-all duration-300 hover:bg-neutral-900/75 hover:border-amber-500/30 hover:-translate-y-1 active:scale-90">
+      <div className="flex items-start justify-between">
+        <div className="absolute -bottom-5 -right-3 group-hover:bottom-0 group-hover:right-0 transition-all duration-300">
+          <h1 className="text-white/5 group-hover:text-amber-500/20 font-heading text-9xl">
+            {teamName.charAt(0).toUpperCase()}
+          </h1>
+        </div>
+
+        <h3 className="font-semibold text-white">{teamName}</h3>
+
+        <ChevronRight
+          size={16}
+          className="text-neutral-500 transition-transform group-hover:translate-x-1"
+        />
+      </div>
+
+      <div className="mt-4 flex items-center gap-2 text-sm text-neutral-400">
+        <Users size={16} />
+        <span>{members} members</span>
+      </div>
+    </div>
+  );
+}
--- app/components/team/TeamPage.tsx
+++ app/components/team/TeamPage.tsx
@@ -1,5 +1,6 @@
 import { ChevronRight, Users } from "lucide-react";
 import AppNavbar from "../ui/app-navbar/AppNavbar";
+import TeamCard from "./TeamCard";
 
 const TEAMS = [
   { id: 1, teamName: "Flaw Team", members: 2 },
@@ -24,30 +25,7 @@ export default function TeamPage() {
 
       <div className="h-fit w-full p-5 flex flex-row flex-wrap gap-3.5 items-center">
         {TEAMS.map((team, index) => (
-          <div
-            key={index}
-            className="relative font-sans group overflow-hidden w-full md:max-w-70 max-h-40 min-h-30 cursor-pointer rounded-2xl border border-neutral-800 bg-neutral-900 p-4 transition-all duration-300 hover:bg-neutral-900/75 hover:border-amber-500/30 hover:-translate-y-1 active:scale-90"
-          >
-            <div className="flex items-start justify-between">
-              <div className="absolute -bottom-5 -right-3 group-hover:bottom-0 group-hover:right-0 transition-all duration-300">
-                <h1 className="text-white/5 group-hover:text-amber-500/20 font-heading text-9xl">
-                  {team.teamName.charAt(0).toUpperCase()}
-                </h1>
-              </div>
-
-              <h3 className="font-semibold text-white">{team.teamName}</h3>
-
-              <ChevronRight
-                size={16}
-                className="text-neutral-500 transition-transform group-hover:translate-x-1"
-              />
-            </div>
-
-            <div className="mt-4 flex items-center gap-2 text-sm text-neutral-400">
-              <Users size={16} />
-              <span>{team.members} members</span>
-            </div>
-          </div>
+          <TeamCard {...team} key={index} />
         ))}
       </div>
     </div>
