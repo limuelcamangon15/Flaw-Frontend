70dc2ec8ce8c42192ab393d6c637dcd2f23ce7a4
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-11 21:56:10 +0800

    update: added bug card

---
+++ app/components/team/BugCard.tsx
@@ -0,0 +1,12 @@
+export default function BugCard() {
+  return (
+    <div className="relative font-sans group overflow-hidden w-full md:max-w-full max-h-40 min-h-30 cursor-pointer rounded-2xl border border-neutral-800 bg-neutral-900 p-4 transition-all duration-300 hover:bg-neutral-900/75 hover:border-amber-500/30 hover:-translate-y-1 active:scale-90">
+      <p>Limuel Camangon</p>
+
+      <p>
+        Lorem, ipasda das das das dasdasdsasdasdad adadaemo, provident et autem
+        nostrum incidunt.
+      </p>
+    </div>
+  );
+}
--- app/components/team/TeamWorkspace.tsx
+++ app/components/team/TeamWorkspace.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import BugCard from "./BugCard";
 
 export default function TeamWorkspace() {
-  return <div className="w-full h-full pt-20">TeamWorkspace</div>;
+  return (
+    <div className="w-full h-full pt-20 p-5">
+      <h1 className="font-heading text-3xl">Concurrent Team</h1>
+
+      <BugCard />
+    </div>
+  );
 }
