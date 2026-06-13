74f17a2a62acc3aa8e4c7c0c32d08db76aedc432
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-13 23:59:22 +0800

    update: added more bug card sample

--- app/components/team/TeamWorkspace.tsx
+++ app/components/team/TeamWorkspace.tsx
@@ -15,6 +15,8 @@ export default function TeamWorkspace() {
         <BugCard />
         <BugCard />
         <BugCard />
+        <BugCard />
+        <BugCard />
       </div>
 
       <BugDetails
